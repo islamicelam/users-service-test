42a07dd80b7e313b1a6b8cc4fa06063140f8b88d
Islam Velikhanli <islamvelikhanli@Islams-iMac.local>
2026-04-27 11:24:22 +0400

    added User entity with migration

---
+++ src/database/migrations/1777274525953-CreateUsersTable.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateUsersTable1777274525953 implements MigrationInterface {
+    name = 'CreateUsersTable1777274525953'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TYPE "public"."users_role_enum" AS ENUM('admin', 'user')`);
+        await queryRunner.query(`CREATE TYPE "public"."users_status_enum" AS ENUM('active', 'blocked')`);
+        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "fullname" character varying(255) NOT NULL, "birthDate" date NOT NULL, "email" character varying(255) NOT NULL, "password" character varying(255) NOT NULL, "role" "public"."users_role_enum" NOT NULL DEFAULT 'user', "status" "public"."users_status_enum" NOT NULL DEFAULT 'active', "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_97672ac88f789774dd47f7c8be" ON "users" ("email") `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX "public"."IDX_97672ac88f789774dd47f7c8be"`);
+        await queryRunner.query(`DROP TABLE "users"`);
+        await queryRunner.query(`DROP TYPE "public"."users_status_enum"`);
+        await queryRunner.query(`DROP TYPE "public"."users_role_enum"`);
+    }
+
+}
---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,34 @@
+import { Column, CreateDateColumn, Entity, Index, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { UserRole } from "../enums/user-role.enum";
+import { UserStatus } from "../enums/user-status.enum";
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn('uuid') 
+    id!: string; 
+
+    @Column({type: 'varchar', length: 255, name: 'fullname'})
+    fullName!: string;
+
+    @Column({type: 'date'})
+    birthDate!: Date;
+
+    @Index({unique: true})
+    @Column({type: 'varchar', length: 255})
+    email!: string;
+
+    @Column({type: 'varchar', length:255, select: false}) //don't return on SELECT operations
+    password!: string;
+
+    @Column({type: 'enum', enum: UserRole, default: UserRole.USER})
+    role!: UserRole;
+
+    @Column({type: 'enum', enum: UserStatus, default: UserStatus.ACTIVE})
+    status!: UserStatus;
+
+    @CreateDateColumn({name: 'created_at'})
+    createdAt!: Date;
+
+    @UpdateDateColumn({name: 'updated_at'})
+    updatedAt!: Date;
+}
